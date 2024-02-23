4b3f45eefe85cecb4f1ba848b3854533e8e7fef4
Huang Harvey <mail.bing@qq.com>
2024-02-23 17:28:29 +0800

    alt+q全局快捷键

--- DotNetAssistant/Views/MainWindow.xaml.cs
+++ DotNetAssistant/Views/MainWindow.xaml.cs
@@ -7,6 +7,8 @@ using System.Windows.Data;
 using System.Windows.Media.Imaging;
 using System.Windows.Interop;
 using DotNetAssistant.ViewModels;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 
 namespace DotNetAssistant.Views
 {
@@ -15,16 +17,92 @@ namespace DotNetAssistant.Views
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             e.Cancel = true;
             if (GlobalVars.ShowConfirm("确认退出软件？"))
                 Environment.Exit(0);
+        }
+
+        private const int WM_HOTKEY = 0x0312;
+        private const int MOD_ALT = 0x0001;
+        private const int MOD_CONTROL = 0x0002;
+        private const int MOD_SHIFT = 0x0004;
+        private const int VK_Q = 0x51;
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        [DllImport("user32.dll")]
+        private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int x, int y, int cx, int cy, uint flags);
+
+        private const uint SWP_SHOWWINDOW = 0x0040;
+        private const uint SWP_NOSIZE = 0x0001;
+        private const uint SWP_NOMOVE = 0x0002;
+
+        private static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+        private static readonly IntPtr HWND_NOTOPMOST = new IntPtr(-2);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+            Closing += MainWindow_Closing;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // 注册全局快捷键
+            IntPtr hWnd = new WindowInteropHelper(this).Handle;
+            RegisterHotKey(hWnd, 1, MOD_ALT, VK_Q);
+            ComponentDispatcher.ThreadPreprocessMessage += ComponentDispatcher_ThreadPreprocessMessage;
+            StateChanged += MainWindow_StateChanged;
+        }
+
+        private void MainWindow_StateChanged(object sender, EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+                Hide();
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // 注销全局快捷键
+            IntPtr hWnd = new WindowInteropHelper(this).Handle;
+            UnregisterHotKey(hWnd, 1);
+        }
+
+        private void ComponentDispatcher_ThreadPreprocessMessage(ref MSG msg, ref bool handled)
+        {
+            // 处理接收到的消息
+            if (msg.message == WM_HOTKEY && msg.wParam.ToInt32() == 1)
+            {
+                // 当接收到全局快捷键消息时，执行相应的操作
+                if (WindowState == WindowState.Minimized)
+                {
+                    Show();
+                    WindowState = WindowState.Normal;
+
+                    SetWindowPos(new WindowInteropHelper(this).Handle, HWND_TOPMOST, 0, 0, 0, 0, SWP_SHOWWINDOW | SWP_NOSIZE | SWP_NOMOVE);
+
+                    SetWindowPos(new WindowInteropHelper(this).Handle, HWND_NOTOPMOST, 0, 0, 0, 0, SWP_SHOWWINDOW | SWP_NOSIZE | SWP_NOMOVE);
+                }
+                else
+                {
+                    WindowState = WindowState.Minimized;
+                }
+
+                handled = true;
+            }
+        }
+
+        private void NotifyIcon_MouseDoubleClick(object sender, RoutedEventArgs e)
+        {
+            Show();
+            WindowState = WindowState.Normal;
         }
     }
 
